49608ce8a2f85b7fe4975de3457e4d78e27a5864
Marc Moten <marcmotenii@gmail.com>
2021-06-15 19:48:44 -0500

    testing API call

--- assets/news.js
+++ assets/news.js
@@ -1,5 +1,11 @@
 var topStories = document.getElementById("topStoriesList")
 
-function newsAPI() {
-
+function getNews() {
+    var httpRQ = new XMLHttpRequest();
+    httpRQ.open('GET', 'https://newsdata.io/api/1/news?apikey=pub_381b5930794fc58a9ddf44f4d6585994a39')
+    httpRQ.onload = function () {
+        console.log(httpRQ.response);
+    };
+    httpRQ.send();
 }
+getNews();
