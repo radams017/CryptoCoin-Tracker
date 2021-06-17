efa546e82d567adba0da4508c017a1ba59635eac
Ryan Adams <ryanadams14@gmail.com>
2021-06-17 18:40:07 -0500

    adding JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
-function coinAPI(){
+// var inputEl = $('#')
+
+// function selectionHandler() {
+//     let coin = inputEl.val();
     
-}
+    
+// }
+let filteResult = null;
+
+function getCoinObj() {
+    const apiUrl = 'https://api.coingecko.com/api/v3/coins/list'
+    const word = 
+    fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (coinsObj) {
+        if (coinsObj.length > 0) {
+            filteResult = coinsObj.filter(function (item) {
+               return item.name.toLowerCase().includes(word.toLowerCase());
+            }); 
+            console.log(filteResult);
+        }
+    });
+};
+
+getCoinObj();
