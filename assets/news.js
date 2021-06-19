0db41492e77bb104161827d1038a4440a838256a
Marc Moten <marcmotenii@gmail.com>
2021-06-19 11:26:12 -0500

    changed the news API to usearch

--- assets/news.js
+++ assets/news.js
@@ -7,21 +7,22 @@ var story5 = document.getElementById("story5");
 function getNews() {
     var httpRQ = new XMLHttpRequest();
 
-    httpRQ.open('GET', 'https://newsapi.org/v2/everything?q=crypto&language=en&from=2021-06-01&apiKey=c25691dd0dd4443f89cab34c230a7ced')
+    httpRQ.open('GET', "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=crypto&pageNumber=1&pageSize=6&autoCorrect=true&safeSearch=true&fromPublishedDate=2021-06-01&toPublishedDate=null");
+    httpRQ.setRequestHeader("x-rapidapi-key", "31821ae77emsh5e7fa0996d8e41bp1cba9cjsn209747bed999");
+    httpRQ.setRequestHeader("x-rapidapi-host", "contextualwebsearch-websearch-v1.p.rapidapi.com");
     httpRQ.onload = function () {
         var data = JSON.parse(httpRQ.response);
+        story1.textContent = data.value[1].title;
+        story2.textContent = data.value[2].title;
+        story3.textContent = data.value[3].title;
+        story4.textContent = data.value[4].title;
+        story5.textContent = data.value[5].title;
 
-        story1.textContent = data.articles[0].title;
-        story2.textContent = data.articles[1].title;
-        story3.textContent = data.articles[2].title;
-        story4.textContent = data.articles[3].title;
-        story5.textContent = data.articles[4].title;
-
-        story1.setAttribute("href", data.articles[0].url);
-        story2.setAttribute("href", data.articles[1].url);
-        story3.setAttribute("href", data.articles[2].url);
-        story4.setAttribute("href", data.articles[3].url);
-        story5.setAttribute("href", data.articles[4].url);
+        story1.setAttribute("href", data.value[1].url);
+        story2.setAttribute("href", data.value[2].url);
+        story3.setAttribute("href", data.value[3].url);
+        story4.setAttribute("href", data.value[4].url);
+        story5.setAttribute("href", data.value[5].url);
     };
 
     httpRQ.send();
