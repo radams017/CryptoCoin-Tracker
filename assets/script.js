602f14ce0b3269ea19fed3236280cb3eead7cc13
Ryan Adams <ryanadams14@gmail.com>
2021-06-19 13:23:46 -0500

    added autocomplete and collection for currencies

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,59 @@
 $(function () {
 
-    let filteResult = null;
     let coinsVal = $('#coins');
-
-    function getCoinObj() {
-        const apiUrl = 'https://api.coingecko.com/api/v3/coins/list'
-        const word = ''
-        fetch(apiUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (coinsObj) {
-                if (coinsObj.length > 0) {
-                    filteResult = coinsObj.filter(function (item) {
-                        return item.name.toLowerCase().includes(word.toLowerCase());
-                    });
-                    console.log(filteResult);
-                }
-            });
-    };
 
     const coinsNamesArray = coinsArray.map(function (item) {
         return item.name;
     });
 
-
+    //autocomplete form dropdown
     $("#coins").autocomplete({
         source: coinsNamesArray
     });
 
-
-
+    //handle coin form submission
     $('#coin-select').submit((e) => {
         e.preventDefault();
-
         let coinId = findItemId(coinsVal.val());
         console.log(coinId);
         coinsVal.val('');
+        getCoinData(coinId)
     });
 
-    //capture the item the user clickeck over
-    //name
+    //handle currency form submission
+    $('#currency-select').submit((e) => {
+        e.preventDefault();
+        let selectedCurrency = $('#currencies').val();
+        console.log(selectedCurrency);
+    })
 
-    //find the name clicked in the original array (coinsArray) you will get the id
-
-    const findItemId = function (choosenItem) {
+    //captures submission ID
+    const findItemId = function (chosenItem) {
         const objectSelected = coinsArray.find(function (arrayItem) {
-            return arrayItem.name === choosenItem;
+            return arrayItem.name === chosenItem;
         });
-
         return objectSelected.id;
-
     }
+
+    //get coin market data
+    function getCoinData(chosenCoinID) {
+        let apiURL = `https://api.coingecko.com/api/v3/coins/${chosenCoinID}`
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    //get supported vs currencies
+    function getCurrencyOptions() {
+        let apiURL = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies'
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(data => {
+                $('#currencies').autocomplete({
+                    source: data
+                });
+            });
+    };
+
+    getCurrencyOptions()
 });
