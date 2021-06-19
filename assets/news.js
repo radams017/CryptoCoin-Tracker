072b9d03f2f0449f934980a524e612372fdba3fc
Ryan Adams <ryanadams14@gmail.com>
2021-06-19 10:28:46 -0500

    created js file for coin list

--- assets/news.js
+++ assets/news.js
@@ -1,33 +1,59 @@
-var topStories = document.getElementById("topStoriesList");
-var story1 = document.getElementById("story1");
-var story2 = document.getElementById("story2");
-var story3 = document.getElementById("story3");
-var story4 = document.getElementById("story4");
-var story5 = document.getElementById("story5");
+// document.addEventListener('DOMContentLoaded', function () {
 
-function getNews() {
-    var httpRQ = new XMLHttpRequest();
+// var topStories = document.getElementById("topStoriesList");
+// var story1 = document.getElementById("story1");
+// var story2 = document.getElementById("story2");
+// var story3 = document.getElementById("story3");
+// var story4 = document.getElementById("story4");
+// var story5 = document.getElementById("story5");
 
-    httpRQ.open('GET', 'https://newsdata.io/api/1/news?apikey=pub_381b5930794fc58a9ddf44f4d6585994a39&country=us&language=en&q=Cryptocurrency')
 
-    httpRQ.onload = function () {
-        var data = JSON.parse(httpRQ.response);
+// function getNews() {
+//     var httpRQ = new XMLHttpRequest();
 
-        story1.textContent = data.results[0].title;
-        story2.textContent = data.results[1].title;
-        story3.textContent = data.results[2].title;
-        story4.textContent = data.results[3].title;
-        story5.textContent = data.results[4].title;
+//     httpRQ.open('GET', 'https://newsdata.io/api/1/news?apikey=pub_381b5930794fc58a9ddf44f4d6585994a39&country=us&language=en&q=Cryptocurrency')
 
-        story1.setAttribute("href", data.results[0].link);
-        story2.setAttribute("href", data.results[1].link);
-        story3.setAttribute("href", data.results[2].link);
-        story4.setAttribute("href", data.results[3].link);
-        story5.setAttribute("href", data.results[4].link);
+//     httpRQ.onload = function () {
+//         var data = JSON.parse(httpRQ.response);
 
-    };
+//         story1.textContent = data.results[0].title;
+//         story2.textContent = data.results[1].title;
+//         story3.textContent = data.results[2].title;
+//         story4.textContent = data.results[3].title;
+//         story5.textContent = data.results[4].title;
 
-    httpRQ.send();
+//         story1.setAttribute("href", data.results[0].link);
+//         story2.setAttribute("href", data.results[1].link);
+//         story3.setAttribute("href", data.results[2].link);
+//         story4.setAttribute("href", data.results[3].link);
+//         story5.setAttribute("href", data.results[4].link);
+
+//     };
+
+//     httpRQ.send();
+// }
+// // getNews();
+
+// loadData();
+// });
+var apiKey = 'pub_381b5930794fc58a9ddf44f4d6585994a39'
+
+document.addEventListener('DOMContentLoaded', function () {
+loadData();
+})
+var loadData = function() {
+    fetch(`https://newsdata.io/api/1/news?apikey=${apiKey}&country=us&language=en&q=Cryptocurrency`, {
+        headers: {
+            'Access-Control-Allow-Origin': 'localhost',
+            'Access-Control-Allow-Credentials': 'true',
+        }
+    }).then(function(data){
+        console.log(data)}
+    ).catch(function(error){
+        console.log(error)
+    });
+//   .then(response => response.json())
+//   .then(data => console.log(data));
 }
-getNews();
 
+//oadData()
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,8 @@
-// var inputEl = $('#')
-
-// function selectionHandler() {
-//     let coin = inputEl.val();
-    
-    
-// }
 let filteResult = null;
 
 function getCoinObj() {
     const apiUrl = 'https://api.coingecko.com/api/v3/coins/list'
-    const word = 
+    const word = ''
     fetch(apiUrl)
     .then(function (response) {
       return response.json();
