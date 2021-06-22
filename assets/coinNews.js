3520ffe83ef0620dafbf3c75cc8090297e5ed81e
Mamii868 <83027431+Mamii868@users.noreply.github.com>
2021-06-21 23:27:55 -0500

    Resources Page (#14)
    * added anchor tags to buttons
    
    * added news.js file and beginning API call function
    
    * testing API call
    
    * created list for top stories
    
    * created function to get latest news from API and append to a list
    
    * deleted for loop for console log
    
    * changed getNews function to pull results using keyword "Cryptocurrency"
    
    * adding JS
    
    * manually merged index files
    
    * slight update
    
    * changed API to newsapi.org
    
    * deleted unused variable and console.log
    
    * created js file for coin list
    
    * changed the news API to usearch
    
    * added dropdown and autocomplete and id mapping
    
    * function for pulling up news on selected currency
    
    * resources page
    
    * added autocomplete and collection for currencies
    
    * added paragraph explaining how to use the site
    
    Co-authored-by: Ryan Adams <ryanadams14@gmail.com>

--- assets/coinNews.js
+++ assets/coinNews.js
@@ -1,13 +1,11 @@
-var story1 = document.getElementById("story1");
-var story2 = document.getElementById("story2");
-var story3 = document.getElementById("story3");
-var story4 = document.getElementById("story4");
-var story5 = document.getElementById("story5");
-
+var story1 = "#"
+var story2 = "#"
+var story3 = "#"
+var currency = "#"
 function getNews() {
     var httpRQ = new XMLHttpRequest();
 
-    httpRQ.open('GET', "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=crypto&pageNumber=1&pageSize=6&autoCorrect=true&safeSearch=true&fromPublishedDate=2021-06-01&toPublishedDate=null");
+    httpRQ.open('GET', "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=" + currency + "&pageNumber=1&pageSize=6&autoCorrect=true&safeSearch=true&fromPublishedDate=2021-06-01&toPublishedDate=null");
     httpRQ.setRequestHeader("x-rapidapi-key", "31821ae77emsh5e7fa0996d8e41bp1cba9cjsn209747bed999");
     httpRQ.setRequestHeader("x-rapidapi-host", "contextualwebsearch-websearch-v1.p.rapidapi.com");
     httpRQ.onload = function () {
@@ -15,19 +13,14 @@ function getNews() {
         story1.textContent = data.value[1].title;
         story2.textContent = data.value[2].title;
         story3.textContent = data.value[3].title;
-        story4.textContent = data.value[4].title;
-        story5.textContent = data.value[5].title;
 
         story1.setAttribute("href", data.value[1].url);
         story2.setAttribute("href", data.value[2].url);
         story3.setAttribute("href", data.value[3].url);
-        story4.setAttribute("href", data.value[4].url);
-        story5.setAttribute("href", data.value[5].url);
+
     };
 
     httpRQ.send();
 }
 
 getNews();
-
-//oadData()
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,59 @@
-function coinAPI(){
-    
-}
+$(function () {
+
+    let coinsVal = $('#coins');
+
+    const coinsNamesArray = coinsArray.map(function (item) {
+        return item.name;
+    });
+
+    //autocomplete form dropdown
+    $("#coins").autocomplete({
+        source: coinsNamesArray
+    });
+
+    //handle coin form submission
+    $('#coin-select').submit((e) => {
+        e.preventDefault();
+        let coinId = findItemId(coinsVal.val());
+        console.log(coinId);
+        coinsVal.val('');
+        getCoinData(coinId)
+    });
+
+    //handle currency form submission
+    $('#currency-select').submit((e) => {
+        e.preventDefault();
+        let selectedCurrency = $('#currencies').val();
+        console.log(selectedCurrency);
+    })
+
+    //captures submission ID
+    const findItemId = function (chosenItem) {
+        const objectSelected = coinsArray.find(function (arrayItem) {
+            return arrayItem.name === chosenItem;
+        });
+        return objectSelected.id;
+    }
+
+    //get coin market data
+    function getCoinData(chosenCoinID) {
+        let apiURL = `https://api.coingecko.com/api/v3/coins/${chosenCoinID}`
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    //get supported vs currencies
+    function getCurrencyOptions() {
+        let apiURL = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies'
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(data => {
+                $('#currencies').autocomplete({
+                    source: data
+                });
+            });
+    };
+
+    getCurrencyOptions()
+});
