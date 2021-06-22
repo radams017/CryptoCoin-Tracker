179fa93c288377718ef1c132bbe226a624e405c0
Ryan Adams <ryanadams14@gmail.com>
2021-06-22 00:45:48 -0500

    finished currency symbol mapping

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,67 @@
 $(function () {
     let coinsVal = $('#coins');
     const currencySigns = {
-        "usd": "$",
-        "aud": "$",
-        "eth": "ETH",
+        "btc": 'BTC ',
+        "eth": 'ETH ',
+        "ltc": 'LTC ',
+        "bch": 'BCH ',
+        "bnb": 'BNB ',
+        "eos": 'EOS ',
+        "xrp": 'XRP ',
+        "xlm": 'XLM ',
+        "link": 'LINK ',
+        "dot": 'DOT ',
+        "yfi": 'YFI ',
+        "usd": '$',
+        "aed": 'د.إ',
+        "ars": '$',
+        "aud": '$',
+        "bdt": '৳',
+        "bhd": 'د.ب',
+        "bmd": '$',
+        "brl": 'R$',
+        "cad": '$',
+        "chf": 'CHF ',
+        "clp": '$',
+        "cny": '¥',
+        "czk": 'Kč ',
+        "dkk": 'kr ',
+        "eur": '€',
+        "gbp": '£',
+        "hkd": '$',
+        "huf": 'Ft ',
+        "idr": 'Rp ',
+        "ils": '₪',
+        "inr": '₹',
+        "jpy": '¥',
+        "krw": '₩',
+        "kwd": 'د.ك',
+        "lkr": '₨',
+        "mmk": 'K ',
+        "mxn": '$',
+        "myr": 'RM ',
+        "ngn": '₦',
+        "nok": 'kr ',
+        "nzd": '$',
+        "php": '₱',
+        "pkr": '₨',
+        "pln": 'zł',
+        "rub": '₽',
+        "sar": '﷼',
+        "sek": 'kr ',
+        "sgd": '$',
+        "thb": '฿',
+        "try": '&#8378',
+        "twd": 'NT$',
+        "uah": '₴',
+        "vef": 'Bs',
+        "vnd": '₫',
+        "zar": 'R',
+        "xdr": 'XDR ',
+        "xag": 'XAG ',
+        "xau": 'XAU ',
+        "bits": 'BITS ',
+        "sats": 'SATS'
     }
 
     const coinsNamesArray = coinsArray.map(function (item) {
@@ -49,23 +107,33 @@ $(function () {
 
     //paste market data to page
     function appendMarketData(coin, currency) {
+        let vsCurrency = getVsCurrency().toUpperCase();
         $('#results-container').empty().append(`
         </br></br>
         <div class='ui raised segment'>
-        <h1 id='current-coin'>${coin.name}</h1>
-
+        <div class='ui two column grid'>
+            <div class='row'>
+                <div class='column'>
+                    <h1 id='current-coin'>${coin.name}</h1>
+                </div>
+                <div class='column'>
+                    <h2 id='comparison-currency'>Vs. Currency: ${vsCurrency}</h2>
+                </div>
+            </div>
+        </div>
         <div id='coin-market-data' class='ui two column grid'>
             <div class='row'>
                 <div class="column">
-                    <h3 id='current-price'>Price:</h3><p>${currencySigns[currency]}${coin.market_data.current_price[currency]}</p>
+                    <h3 id='current-price'>Current Price:</h3><p>${currencySigns[currency]}${coin.market_data.current_price[currency]}</p>
                     <h3 id='market-cap'>Market Cap:</h3><p>${currencySigns[currency]}${coin.market_data.market_cap[currency]}</p>
-                    <h3 id='circulating-supply'>Circulating Supply:</h3>
+                    <h3 id='total-supply'>Total Supply:</h3><p>${coin.market_data.total_supply}</p>
+                    <h3 id='circulating-supply'>Circulating Supply:</h3><p>${coin.market_data.circulating_supply} / ${coin.market_data.total_supply}</p>
                 </div>
                 <div class='column'>
                     <h3 id='24hr-high'>24hr High:</h3><p>${currencySigns[currency]}${coin.market_data.high_24h[currency]}</p>
                     <h3 id='24hr-low'>24hr Low:</h3><p>${currencySigns[currency]}${coin.market_data.low_24h[currency]}</p>
-                    <h3 id='ATH'>All Time High:</h3><p>${currencySigns[currency]}${coin.market_data.ath[currency]}
-                    <h3 id='ATL'>All Time Low:</h3><p>${currencySigns[currency]}${coin.market_data.atl[currency]}
+                    <h3 id='ATH'>All Time High:</h3><p>${currencySigns[currency]}${coin.market_data.ath[currency]}</p>
+                    <h3 id='ATL'>All Time Low:</h3><p>${currencySigns[currency]}${coin.market_data.atl[currency]}</p>
                 </div>
             </div>
         </div>
